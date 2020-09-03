8e511f26c3a20dea6c2f4062662d2c08b2a32044
Messias Junior <messiaslima.03@gmail.com>
2020-09-03 09:47:46 +0100

    chore: setup action bar

--- app/src/main/java/com/messiasjunior/codewarsv2/presentation/MainActivity.kt
+++ app/src/main/java/com/messiasjunior/codewarsv2/presentation/MainActivity.kt
@@ -2,11 +2,32 @@ package com.messiasjunior.codewarsv2.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.messiasjunior.codewarsv2.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavHost) as NavHostFragment
+        val navigationController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navigationController.graph)
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navigationController = findNavController(R.id.mainNavHost)
+        return navigationController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
