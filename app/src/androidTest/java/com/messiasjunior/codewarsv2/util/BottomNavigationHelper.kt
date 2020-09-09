4dae75d2b566c27c98b1930440a16cf08dab3ff0
Messias Junior <messiaslima.03@gmail.com>
2020-09-09 13:47:01 +0100

    refactor: HasCheckedItemMatcher

--- app/src/androidTest/java/com/messiasjunior/codewarsv2/util/BottomNavigationHelper.kt
+++ app/src/androidTest/java/com/messiasjunior/codewarsv2/util/BottomNavigationHelper.kt
@@ -6,42 +6,48 @@ import android.view.View
 import androidx.test.espresso.matcher.BoundedMatcher
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.hamcrest.Description
-import org.hamcrest.Matcher
 
-fun hasCheckedItem(id: Int): Matcher<View> {
-    return object :
-        BoundedMatcher<View, BottomNavigationView>(BottomNavigationView::class.java) {
-        var checkedIds: MutableSet<Int> = HashSet()
-        var itemFound = false
-        var triedMatching = false
-        override fun describeTo(description: Description) {
-            if (!triedMatching) {
-                description.appendText("BottomNavigationView")
-                return
-            }
-            description.appendText("BottomNavigationView to have a checked item with id=")
-            description.appendValue(id)
-            if (itemFound) {
-                description.appendText(", but selection was=")
-                description.appendValue(checkedIds)
-            } else {
-                description.appendText(", but it doesn't have an item with such id")
-            }
+class HasCheckedItemMatcher(
+    private val id: Int
+) : BoundedMatcher<View, BottomNavigationView>(BottomNavigationView::class.java) {
+    private val checkedIds: MutableSet<Int> = HashSet()
+    private var itemFound = false
+    private var triedMatching = false
+
+    override fun describeTo(description: Description) {
+        if (!triedMatching) {
+            description.appendText("BottomNavigationView")
+            return
         }
 
-        override fun matchesSafely(navigationView: BottomNavigationView): Boolean {
-            triedMatching = true
-            val menu: Menu = navigationView.menu
-            for (i in 0 until menu.size()) {
-                val item: MenuItem = menu.getItem(i)
-                if (item.isChecked) {
-                    checkedIds.add(item.itemId)
-                }
-                if (item.itemId == id) {
-                    itemFound = true
-                }
-            }
-            return checkedIds.contains(id)
+        description.appendText("BottomNavigationView to have a checked item with id=")
+        description.appendValue(id)
+
+        if (itemFound) {
+            description.appendText(", but selection was=")
+            description.appendValue(checkedIds)
+        } else {
+            description.appendText(", but it doesn't have an item with such id")
         }
     }
+
+    override fun matchesSafely(navigationView: BottomNavigationView): Boolean {
+        triedMatching = true
+
+        val menu: Menu = navigationView.menu
+
+        for (i in 0 until menu.size()) {
+            val item: MenuItem = menu.getItem(i)
+            if (item.isChecked) {
+                checkedIds.add(item.itemId)
+            }
+            if (item.itemId == id) {
+                itemFound = true
+            }
+        }
+
+        return checkedIds.contains(id)
+    }
 }
+
+fun hasCheckedItem(id: Int) = HasCheckedItemMatcher(id)
