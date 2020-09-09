5c5509bd1b2da97a35d933e73d7cc4ccd5f107f2
Messias Junior <messiaslima.03@gmail.com>
2020-09-09 01:16:03 +0100

    refactor: recyclerview matcher

--- app/src/androidTest/java/com/messiasjunior/codewarsv2/util/RecyclerViewMatcher.kt
+++ app/src/androidTest/java/com/messiasjunior/codewarsv2/util/RecyclerViewMatcher.kt
@@ -19,49 +19,55 @@ class RecyclerViewMatcher(private val recyclerViewId: Int) {
     }
 
     private fun atPositionOnView(position: Int, targetViewId: Int): Matcher<View> {
-        return object : TypeSafeMatcher<View>() {
-            var resources: Resources? = null
-            var childView: View? = null
-
-            override fun describeTo(description: Description) {
-                var idDescription = recyclerViewId.toString()
-                if (this.resources != null) {
-                    idDescription = try {
-                        this.resources?.getResourceName(recyclerViewId)!!
-                    } catch (resourceNotFound: NotFoundException) {
-                        String.format(
-                            "%s (resource name not found)",
-                            Integer.valueOf(recyclerViewId)
-                        )
-                    }
-                }
-
-                description.appendText("with id: $idDescription")
-            }
-
-            override fun matchesSafely(view: View?): Boolean {
-                resources = view?.resources
-
-                if (childView == null) {
-                    val recyclerView = view?.rootView?.findViewById(recyclerViewId) as RecyclerView
-                    childView = if (recyclerView.id == recyclerViewId) {
-                        recyclerView.findViewHolderForAdapterPosition(position)!!.itemView
-                    } else {
-                        return false
-                    }
-                }
-
-                return if (targetViewId == -1) {
-                    view == childView
-                } else {
-                    val targetView = childView!!.findViewById<View>(targetViewId)
-                    view == targetView
-                }
-            }
-        }
+        return RecyclerViewTypeSafeMatcher(recyclerViewId, position, targetViewId)
     }
 }
 
 fun withRecyclerView(recyclerViewId: Int): RecyclerViewMatcher {
     return RecyclerViewMatcher(recyclerViewId)
+}
+
+class RecyclerViewTypeSafeMatcher(
+    private val recyclerViewId: Int,
+    private val position: Int,
+    private val targetViewId: Int
+) : TypeSafeMatcher<View>() {
+    private var resources: Resources? = null
+    private var childView: View? = null
+
+    override fun describeTo(description: Description) {
+        var idDescription = recyclerViewId.toString()
+        if (this.resources != null) {
+            idDescription = try {
+                this.resources?.getResourceName(recyclerViewId)!!
+            } catch (resourceNotFound: NotFoundException) {
+                String.format(
+                    "%s (resource name not found)",
+                    Integer.valueOf(recyclerViewId)
+                )
+            }
+        }
+
+        description.appendText("with id: $idDescription")
+    }
+
+    override fun matchesSafely(view: View?): Boolean {
+        resources = view?.resources
+
+        if (childView == null) {
+            val recyclerView = view?.rootView?.findViewById(recyclerViewId) as RecyclerView
+            childView = if (recyclerView.id == recyclerViewId) {
+                recyclerView.findViewHolderForAdapterPosition(position)!!.itemView
+            } else {
+                return false
+            }
+        }
+
+        return if (targetViewId == -1) {
+            view == childView
+        } else {
+            val targetView = childView!!.findViewById<View>(targetViewId)
+            view == targetView
+        }
+    }
 }
